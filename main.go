9d842502d97ef57d341ede0899bee02a6750723b
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-07-21 16:16:28 -0300

    CRUD operations on products

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"runtime"
 	"github.com/gin-gonic/gin"
 )
 
-// I will build two endpoints and handlers
+// Generic endpoint handler
+func endpointHandler(c *gin.Context) {
+	c.String(200, "%s %s", c.Request.Method, c.Request.URL.Path)
+}
 
 func main () {
 	gin.SetMode(gin.ReleaseMode)
@@ -13,18 +15,11 @@ func main () {
 	// router := gin.New()
 	// router.Use(gin.Logger())
 
-
-	// HTTP GET -> RETURN JSON Formatted message
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message":"Hello world!",
-		})
-	})
-
-	// HTTP GET -> RETURN The current OS name in plain text format
-	router.GET("/os", func(c *gin.Context){
-		c.String(200, runtime.GOOS)
-	})
+	router.GET("/products", endpointHandler)
+	router.GET("/products/:productId", endpointHandler)
+	router.POST("/products",endpointHandler)
+	router.PUT("/products/:productId", endpointHandler)
+	router.DELETE("/products/:productId", endpointHandler)
 
 	// Start the http server instance
 	router.Run(":5000")
