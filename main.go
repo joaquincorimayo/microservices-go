de015b4d26adcd445c53a9ebcdb4656e324052fb
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-07-21 22:30:34 -0300

    Add custom middleware

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
+	"log"
 )
+
+// custom middleware intercepts and prints the User-Agent header's value for each HTTP request
+func FindUserAgent() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println(c.GetHeader("User-Agent"))
+		c.Next()
+	}
+}
 
 func main () {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(FindUserAgent())
 	router.GET("/", func(c *gin.Context){
-		c.JSON(200, gin.H{"message":"CORS works!"})
+		c.JSON(200, gin.H{"message":"Middleware works!"})
 	})
 
 	// Start the http server instance
