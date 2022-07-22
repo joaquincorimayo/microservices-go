1c5818eef77da96d825b9d7683fe214f9aed4ca8
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-07-21 22:24:28 -0300

    Enable CORS-middleware

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
-
-type PrintJob struct {
-	JobId int `json:"jobId" binding:"required,gte=10000"`
-	Pages int `json:"pages" binding:"required,gte=1,lte=100"`
-}
 
 func main () {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.POST("/print",func(c *gin.Context) {
-		var p PrintJob
-
-		if err := c.ShouldBindJSON(&p); err != nil {
-			c.JSON(400, gin.H{"error":"Invalid input!"})
-			return
-		}
-		c.JSON(200, gin.H{"message":
-			fmt.Sprintf("PrintJob #%v started!", p.JobId)})
+	router.Use(cors.Default())
+	router.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{"message":"CORS works!"})
 	})
 
 	// Start the http server instance
